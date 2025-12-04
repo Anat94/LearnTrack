60caa703c382515e9402af6b1161d71d004e077b
Anatole BABIN <anatole.babin@visian.fr>
2025-12-04 10:24:17 +0100

    initial commit

---
+++ LearnTrack/LearnTrack/LearnTrackApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearnTrackApp.swift
+//  LearnTrack
+//
+//  Created by anatole on 04/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct LearnTrackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
